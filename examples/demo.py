b762d23596b816aae9f3e01573975cc9b21f4c41
赖勇浩 <lyh@gzqichang.com>
2022-06-21 21:00:51 +0800

    fix: 调整程序结构。

--- examples/demo.py
+++ examples/demo.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 app.secret_key = "florist demo site secret key"
 app.config['MONGODB_SETTINGS'] = {'DB': 'florist-demo'}
 
-from florist.db import init as db_init
-db_init(app)
 
 from florist.site import init as site_init
 site_init(app)
+
+# 业务网站
+from florist.cms import cms_bp
+app.register_blueprint(cms_bp, url_prefix='/')
--- florist/site/__init__.py
+++ florist/site/__init__.py
@@ -1,17 +1,22 @@
-from flask import Blueprint
+# from flask import Blueprint
 
-site_bp = Blueprint('site', __name__)
-
-from ..cms import cms_bp
-site_bp.register_blueprint(cms_bp)
-# from ..contrib.admin import admin_bp
-# site_bp.register_blueprint(admin_bp, url_prefix='/admin')
+# site_bp = Blueprint('site', __name__)
 
 def init(app):
+    # 初始化数据库
+    from ..db import init as db_init
+    db_init(app)
+    # 初始化用户
     from ..user import init as user_init
     user_init(app)
-    
-    from .admin import init as admin_init
-    admin_init(app, url_prefix='/admin')
 
-    app.register_blueprint(site_bp, url_prefix='/')
+    # from ..contrib.admin import admin_bp
+    # site_bp.register_blueprint(admin_bp, url_prefix='/admin')
+    # 初始化管理后台
+    from ..admin import init as admin_init
+    admin_init(app, url_prefix='/admin')
+    # 初始化前台
+    # from ..cms import cms_bp
+    # site_bp.register_blueprint(cms_bp)
+    # # 挂载到app
+    # app.register_blueprint(site_bp, url_prefix='/')
