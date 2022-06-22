b3606732d324d90ecbf15e9ad967636f424ad2f7
赖勇浩 <lyh@gzqichang.com>
2022-06-22 12:31:21 +0800

    fix: 整理配置。

--- examples/settings.py
+++ examples/settings.py
@@ -14,7 +14,4 @@ SECURITY_PASSWORD_SALT = 'a58063c79d92865d79'
 
 # ------ Flask-thumbnails ------
 
-# ------ Florist ------
-FLORIST_ADMIN_EMAIL ='mail@laiyonghao.com'
-FLORIST_ADMIN_PASSWORD = '123123'
-# ------ Florist ------
+from .dev_settings import *
