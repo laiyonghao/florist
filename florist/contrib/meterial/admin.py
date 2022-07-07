b2f04d191617b1be622254c5167f28079bcb57fb
赖勇浩 <lyh@gzqichang.com>
2022-07-07 11:48:15 +0800

    fix: 路径配置。

--- florist/contrib/meterial/admin.py
+++ florist/contrib/meterial/admin.py
@@ -49,7 +49,7 @@ class FileAdmin(OrigFileAdmin):
             self.on_file_upload(directory, path, filename)
 
 
-admin.add_view(FileAdmin('uploaded/', '/uploaded/', name='Uploaded Files'))
+admin.add_view(FileAdmin(admin.app.config['UPLOADED_PATH'], '/uploaded/', name='Uploaded Files'))
 # 通过自定义回调实现相对路径。
 # REF: https://github.com/psolom/RichFilemanager/issues/222
 flaskfilemanager_init(
